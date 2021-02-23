b80bba35ee8d10dd18927e79aa7853856b4d4a8f
FrankBishop <ffbishop3@gmail.com>
2021-02-23 14:54:36 -0800

    Make all home page main elements hidden after search is submitted

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var searchForm = document.querySelector('.search-form');
+var logosRow = document.querySelector('.logo-images');
+var homePageText = document.querySelector('.main-text');
 
 
 searchForm.addEventListener('submit', submitAction);
 
-
+function submitAction(event) {
+  event.preventDefault();
+  console.log('this works');
+  searchForm.className = "hidden";
+  logosRow.className = "hidden";
+  homePageText.className = "hidden";
+}
