e594869d227a80c35fef8896ceebb7af6f5bb6db
FrankBishop <ffbishop3@gmail.com>
2021-02-23 15:49:38 -0800

    search function can now search for user input

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var searchForm = document.querySelector('.search-form');
+var searchBar =  document.querySelector('#game-search')
 var logosRow = document.querySelector('.logo-images');
 var homePageText = document.querySelector('.main-text');
 var searchResults = document.querySelector('.search-results')
@@ -12,13 +13,15 @@ function submitAction(event) {
   logosRow.className = "hidden";
   homePageText.className = "hidden";
   searchForm.className = "search-move";
-  getResults();
+  var searchRequest = searchBar.value;
+  console.log(searchRequest)
+  getResults(searchRequest);
 }
 
-function getResults() {
+function getResults(searchRequest) {
   console.log('this ran');
   var search = new XMLHttpRequest();
-  search.open('GET', 'https://www.cheapshark.com/api/1.0/games?title=batman&limit=10')
+  search.open('GET', 'https://www.cheapshark.com/api/1.0/games?title=' + searchRequest + '&limit=10')
   search.responseType = 'json';
   search.addEventListener('load', function () {
     console.log('status', search.status);
@@ -37,23 +40,3 @@ function getResults() {
   });
   search.send()
 }
-
-// function getUsers() {
-//   var users = document.querySelector('#user-list');
-//   var request = new XMLHttpRequest();
-//   request.open('GET', 'https://jsonplaceholder.typicode.com/users');
-//   request.responseType = 'json';
-//   request.addEventListener('load', function () {
-//     console.log('status', request.status);
-//     console.log('response', request.response);
-//     var i;
-//     for (i = 0; i < this.response.length; i++) {
-//       var list = document.createElement('li');
-//       list.textContent = this.response[i].name;
-//       users.appendChild(list);
-
-//     }
-
-//   })
-//   request.send()
-// }
