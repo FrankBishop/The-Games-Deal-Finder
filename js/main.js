60fdcc5cb15a926e332d69ed83726329706590bb
FrankBishop <ffbishop3@gmail.com>
2021-02-23 15:13:03 -0800

    wrote and tested basic function for XMLHttpRequest request

--- js/main.js
+++ js/main.js
@@ -7,9 +7,41 @@ searchForm.addEventListener('submit', submitAction);
 
 function submitAction(event) {
   event.preventDefault();
-  console.log('this works');
   searchForm.className = "hidden";
   logosRow.className = "hidden";
   homePageText.className = "hidden";
-  searchForm.className = "search-move"
+  searchForm.className = "search-move";
+  getResults();
 }
+
+function getResults() {
+  console.log('this ran');
+  var search = new XMLHttpRequest();
+  search.open('GET', 'https://www.cheapshark.com/api/1.0/games?title=batman&limit=60');
+  search.responseType = 'json';
+  search.addEventListener('load', function () {
+    console.log('status', search.status);
+    console.log('response', search.response);
+  });
+  search.send()
+}
+
+// function getUsers() {
+//   var users = document.querySelector('#user-list');
+//   var request = new XMLHttpRequest();
+//   request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+//   request.responseType = 'json';
+//   request.addEventListener('load', function () {
+//     console.log('status', request.status);
+//     console.log('response', request.response);
+//     var i;
+//     for (i = 0; i < this.response.length; i++) {
+//       var list = document.createElement('li');
+//       list.textContent = this.response[i].name;
+//       users.appendChild(list);
+
+//     }
+
+//   })
+//   request.send()
+// }
